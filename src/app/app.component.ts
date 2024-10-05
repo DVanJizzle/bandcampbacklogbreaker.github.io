c7a4113792d83886fc9146950effc5a1468d7834
Johannes Dahm <johannes.dahm@gmx.de>
2024-10-05 11:23:39 +0200

    Html get functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Console, log } from 'console';
+import { provideHttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +12,37 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'BandcampDecisionMaker';
+  constructor(private http: HttpClient) { }
+
+  title = 'Bandcamp Backlog Breaker';
+  proxyUrl = "https://corsproxy.io/?";
+
+  getUserWishlistData(): void {
+    var input = (document.getElementById("Input") as HTMLInputElement).value;
+    let wishlist: string = this.proxyUrl + "https://bandcamp.com/" + input + "/wishlist";
+
+    var htmlContent: string;
+    this.http.get(wishlist, { responseType: "text" }).subscribe({
+      next: (response: string) => {
+        htmlContent = response;
+        this.getRandomWishlistItem(htmlContent);
+      },
+      error: (error) => {
+        /* Hier soll das Eingabefeld einen Fehler anzeigen*/
+        console.error('Fehler beim Abrufen:', error);
+      },
+      complete: () => {
+        console.log('Anfrage abgeschlossen');
+      }
+    })
+  }
+
+    getRandomWishlistItem(htmlContent: string): void {
+      console.log(htmlContent);
+    }
+  
+
+
 }
